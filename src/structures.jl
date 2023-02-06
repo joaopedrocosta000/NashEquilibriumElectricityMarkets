50b7f4d458be627f8bdc4e1d8edf85e59a57a27a
joaopedrocosta000 <joaopedrocosta000@gmail.com>
2023-02-06 19:45:48 -0300

    created initial structs
    Co-authored-by: Marina Dietze <dietzemarina@users.noreply.github.com>

--- src/structures.jl
+++ src/structures.jl
@@ -1,4 +1,5 @@
 struct ThermalGenerator 
+    number::Int64
     id::Int64 # Dessem
     name::String
     owner::Int64
@@ -13,12 +14,27 @@ struct ThermalGenerator
 end
 
 struct SolarGenerator
+    number::Int64
+    name::String
+    owner::Int64
+    bus::Int64
+    zone::Int64
+    g_max::Vector{Float64}
+    g_min::Vector{Float64}
 end
 
 struct WindGenerator
+    number::Int64
+    name::String
+    owner::Int64
+    bus::Int64
+    zone::Int64
+    g_max::Vector{Float64}
+    g_min::Vector{Float64}
 end
 
 struct HydroGenerator
+    number::Int64
     id::Int64 # Dessem
     name::String
     owner::Int64
@@ -40,19 +56,45 @@ struct HydroGenerator
     ramp_down::Float64
     water_value::Float64 # R$/MWh
     γ::Float64 # R$/hm3
+    inflow::Vector{Float64}
+    cascade::Dict{Int64, Int64} # dicionário com usinas a montante: chave é o id da hidro e valor o tempo de viagem
 end
 
 struct Bus
+    number::Int64
+    name::String
+    zone::Int64
 end
 
 struct Zone
+    number::Int64
+    name::String
 end
 
 struct Line
+    number::Int64
+    from::Int64
+    to::Int64
+    f_max::Float64
+    reactance::Float64
 end
 
 struct Exchange
+    number::Int64
+    from::Int64
+    to::Int64
+    f_max::Float64
 end
 
 struct Company
+    number::Int64
+    name::String
 end
+
+struct Load
+    value::Vector{Float64}
+    bus::Int64
+    zone::Int64
+end
+
+
