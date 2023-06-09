9cb51b71628e37bea10a8641445f320d3400f75f
joaopedrocosta000 <joaopedrocosta000@gmail.com>
2023-06-09 16:44:55 -0300

    Competitive Equilibrium mode finished
    Co-authored-by: Marina Dietze <dietzemarina@users.noreply.github.com>

--- src/objective_functions.jl
+++ src/objective_functions.jl
@@ -3,7 +3,6 @@ function create_future_cost_function(model::Ml, T::Int64, hydro::Vector{HydroGen
     Jgc = length(hydro)
 
     if mode == "nash"
-
         return sum(hydro[j].γ * (model[:v][T, hydro[j].number] - hydro[j].v_initial) for j = 1:Jgc)
     else
         return sum(hydro[j].γ * (model[:v_grid][T, hydro[j].number] - hydro[j].v_initial) for j = 1:Jgc) + 
